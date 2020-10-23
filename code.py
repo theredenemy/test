1a165e9238901b56ddf8463a6d8687fbcc858a9b
theredenemy <34151821+theredenemy@users.noreply.github.com>
2020-10-22 23:10:29 -0700

    Update code.py

--- code.py
+++ code.py
@@ -3,3 +3,8 @@ if Test == 'y':
     print ('your correct she did break the fourth wall')
 else:
     print ('your wrong')
+Test = input ('kit is dead ')
+if Test == 'y':
+    print ('good')
+else:
+    print ('fuck you')
